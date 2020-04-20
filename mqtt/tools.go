74108705bdb41249616013c04e4eb89169fe6517
zilohumberto <zilohumberto@gmail.com>
2020-04-20 09:41:59 -0300

    mqtt tools/utils

---
+++ mqtt/tools.go
@@ -0,0 +1,49 @@
+package mqtttools
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func GetURL(mqttURL string) *url.URL {
+	//MQTT_URL:mqtt://<user>:<pass>@<server>.cloudmqtt.com:<port>/<topic>
+	uri, err := url.Parse(os.Getenv("MQTT_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return uri
+}
+
+func GetTopic(url *url.URL) string {
+	topic := url.Path[1:len(url.Path)]
+	if topic == "" {
+		topic = "test"
+	}
+	return topic
+}
+
+func Connect(opts *mqtt.ClientOptions) mqtt.Client {
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+func CreateClientOptions(clientID string, uri *url.URL) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s", uri.Host))
+	opts.SetUsername(uri.User.Username())
+	password, _ := uri.User.Password()
+	opts.SetPassword(password)
+	opts.SetClientID(clientID)
+	return opts
+}
